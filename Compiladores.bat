4ca0380c86b9d5f177c64d4135ba76210ce60031
Alex Alves <alex7alves10@hotmail.com>
2016-06-28 16:36:06 -0300

    Atualizando arquivo compiladores.bat

--- Compiladores.bat
+++ Compiladores.bat
@@ -4,22 +4,15 @@ title  Compilando
 echo Criando as bibliotecas...
 
 
-cd C:\Portugol-IDE\IDE\lua-5.2.4\src
+cd C:\Portugol-IDE\IDE\lua-5.3.3\src
 mingw32-make.exe mingw
-cd  C:\Portugol-IDE\IDE\lpeglabel-0.12.2
-gcc.exe  -c  *.c  -I C:\Portugol-IDE\IDE\lua-5.2.4\src
-gcc -shared *.o -o lpeglabel.dll -L C:\Portugol-IDE\IDE\lua-5.2.4\src -llua52
+cd  C:\Portugol-IDE\IDE\lpeglabel-0.12.2-2
+gcc.exe  -c  *.c  -I C:\Portugol-IDE\IDE\lua-5.3.3\src
+gcc -shared *.o -o lpeglabel.dll -L C:\Portugol-IDE\IDE\lua-5.3.3\src -llua53
 @copy relabel.lua C:\Portugol-IDE\IDE\portugol-master
-@copy lpeglabel.dll C:\Portugol-IDE\IDE\lua-5.2.4\src
-@SET path = %path% ; C:\Portugol-IDE\IDE\lua-5.2.4\src
-@SETX  path  C:\Portugol-IDE\IDE\lua-5.2.4\src
-
-cd C:\Portugol-IDE\IDE\lua-5.2.4\src
-
-@copy lpeglabel.dll C:\Portugol-IDE\IDE\Executaveis
-@copy lua.exe C:\Portugol-IDE\IDE\Executaveis
-@copy luac.exe C:\Portugol-IDE\IDE\Executaveis
-@copy lua52.dll C:\Portugol-IDE\IDE\Executaveis
+@copy lpeglabel.dll C:\Portugol-IDE\IDE\lua-5.3.3\src
+@SET path = %path% ; C:\Portugol-IDE\IDE\lua-5.3.3\src
+@SETX  path  C:\Portugol-IDE\IDE\lua-5.3.3\src
 
 
 echo ***** Concluido *****
