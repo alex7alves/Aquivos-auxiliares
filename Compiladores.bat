119b4f929e3df8ec7cd50c91e750347d7ec1abb3
Alex Alves <alex7alves10@hotmail.com>
2016-07-16 14:39:48 -0300

    Atualizando compilador.bat

--- Compiladores.bat
+++ Compiladores.bat
@@ -6,7 +6,7 @@ echo Criando as bibliotecas...
 
 cd C:\Portugol-IDE\IDE\lua-5.3.3\src
 mingw32-make.exe mingw
-cd  C:\Portugol-IDE\IDE\lpeglabel-0.12.2-2
+cd  C:\Portugol-IDE\IDE\lpeglabel-master
 gcc.exe  -c  *.c  -I C:\Portugol-IDE\IDE\lua-5.3.3\src
 gcc -shared *.o -o lpeglabel.dll -L C:\Portugol-IDE\IDE\lua-5.3.3\src -llua53
 @copy relabel.lua C:\Portugol-IDE\IDE\portugol-master
