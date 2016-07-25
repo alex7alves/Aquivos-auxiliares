a0519249f2673cc8f1d0a3210785dae33571a8d9
Alex Alves <alex7alves10@hotmail.com>
2016-07-25 11:14:21 -0300

    Mudando para QuoGol IDE

--- Compiladores.bat
+++ Compiladores.bat
@@ -4,16 +4,16 @@ title  Compilando
 echo Criando as bibliotecas...
 echo.
 
-cd C:\Portugol-IDE\IDE\lua-5.3.3\src
+cd C:\QuoGol-IDE\IDE\lua-5.3.3\src
 mingw32-make.exe mingw
-cd  C:\Portugol-IDE\IDE\lpeglabel-master
-gcc.exe  -c  *.c  -I C:\Portugol-IDE\IDE\lua-5.3.3\src
-gcc -shared *.o -o lpeglabel.dll -L C:\Portugol-IDE\IDE\lua-5.3.3\src -llua53
-@copy relabel.lua C:\Portugol-IDE\IDE\portugol-master
-@copy lpeglabel.dll C:\Portugol-IDE\IDE\lua-5.3.3\src
-@SET path = %path% ; C:\Portugol-IDE\IDE\lua-5.3.3\src
-@SETX  path  C:\Portugol-IDE\IDE\lua-5.3.3\src
-cd C:\Portugol-IDE\IDE\lua-5.3.3\src
+cd  C:\QuoGol-IDE\IDE\lpeglabel-master
+gcc.exe  -c  *.c  -I C:\QuoGol-IDE\IDE\lua-5.3.3\src
+gcc -shared *.o -o lpeglabel.dll -L C:\QuoGol-IDE\IDE\lua-5.3.3\src -llua53
+@copy relabel.lua C:\QuoGol-IDE\IDE\portugol-master
+@copy lpeglabel.dll C:\QuoGol-IDE\IDE\lua-5.3.3\src
+@SET path = %path% ; C:\QuoGol-IDE\IDE\lua-5.3.3\src
+@SETX  path  C:\QuoGol-IDE\IDE\lua-5.3.3\src
+cd C:\QuoGol-IDE\IDE\lua-5.3.3\src
 ren lua.exe lua53.exe
 echo.
 echo ***** Concluido *****
